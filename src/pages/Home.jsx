7ec28aa833a772242d93daf9db779597cae2c9e5
Bilek-Petr <p.bilek29@gmail.com>
2024-08-19 11:35:33 +0200

    added stories section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import Banner from "../components/Banner/Banner";
+import Banner from "../components/banners/Banner";
+import Card from "../components/cards/Card";
 
 export default function Home({ data }) {
+  if (!data || data.length === 0) {
+    return <div>No content available.</div>;
+  }
+
   return (
     <>
-      {data.sections.map((section, index) => (
-        <Banner key={index} data={section} index={index} />
-      ))}
+      <section className="about-banners">
+        {data.sections.map((section, index) => (
+          <Banner key={index} data={section} index={index} />
+        ))}
+      </section>
+
+      <section className="stories">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4">
+          {data.stories.map((story, index) => (
+            <Card key={index} cardData={story} />
+          ))}
+        </div>
+      </section>
     </>
   );
 }
