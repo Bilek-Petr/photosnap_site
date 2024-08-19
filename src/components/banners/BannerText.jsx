a8bc212a98fd2eddbafa37f5fbd5c33d1e2d1676
Bilek-Petr <p.bilek29@gmail.com>
2024-08-19 19:48:03 +0200

    refactured code to be cleanier

--- src/components/banners/BannerText.jsx
+++ src/components/banners/BannerText.jsx
@@ -1,29 +1,28 @@
 import Button from "../Button";
 
 export default function BannerText({ bannerData, isFirstBanner }) {
-  // console.log("BannerText data:", bannerData);
+  const containerStyles = `flex items-center justify-center md:w-[38rem] lg:w-[45rem] lg:px-16 ${
+    isFirstBanner
+      ? "bg-pureBlack text-pureWhite"
+      : "bg-pureWhite text-pureBlack"
+  }`;
+
+  const descriptionStyles = `mb-6 text-base opacity-75 ${
+    isFirstBanner ? "text-lightGrey" : "text-pureBlack"
+  }`;
+
+  const buttonColor = isFirstBanner ? "white" : "black";
+
   return (
-    <div
-      className={`flex items-center justify-center md:w-[38rem] lg:w-[45rem] lg:px-16 ${!isFirstBanner ? "bg-pureWhite text-pureBlack" : "bg-pureBlack text-pureWhite"}`}
-    >
+    <div className={containerStyles}>
       <div className="px-7 py-[4.33rem] md:px-12 md:py-24">
         <h2 className="mb-6 text-3xl font-bold uppercase leading-[3rem] tracking-[0.2em] md:text-[2.5rem] md:leading-[3rem] md:tracking-[0.2em]">
           {bannerData.title}
         </h2>
-        <p
-          className={`mb-6 text-base opacity-75 ${!isFirstBanner ? "text-pureBlack" : "text-lightGrey"}`}
-        >
-          {bannerData.description}
-        </p>
-        {isFirstBanner ? (
-          <Button variant="arrow" color="white">
-            {bannerData.cta}
-          </Button>
-        ) : (
-          <Button variant="arrow" color="black">
-            {bannerData.cta}
-          </Button>
-        )}
+        <p className={descriptionStyles}>{bannerData.description}</p>
+        <Button variant="arrow" color={buttonColor}>
+          {bannerData.cta}
+        </Button>
       </div>
     </div>
   );
