73d9527afcb36b21a6e6cacef4360c5b87828adb
Bilek-Petr <p.bilek29@gmail.com>
2024-08-19 10:17:12 +0200

    fixed banner positioning for bigger screen

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,12 +4,11 @@ import BannerImage from "./BannerImage";
 export default function Banner({ data, index }) {
   //so the 2nd banner is styled differently than the others
   const isFirstBanner = index === 0;
-  //every second is row-reverse
-  const isOdd = index % 2 === 0;
+  const isSecond = index % 2 !== 0;
 
   return (
     <div
-      className={`${isOdd ? "md:flex-row-reverse" : "md:flex-row"} flex h-[41rem] h-full w-full flex-col-reverse`}
+      className={`flex h-full w-full flex-col-reverse md:flex-row ${isSecond && "md:flex-row-reverse"}`}
     >
       <BannerText bannerData={data} isFirstBanner={isFirstBanner} />
       <BannerImage bannerData={data} />
