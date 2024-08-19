49cd768ce106414f67333cad69848eefcf2d150e
Bilek-Petr <p.bilek29@gmail.com>
2024-08-19 13:23:25 +0200

    main content of home page finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../components/banners/Banner";
 import Card from "../components/cards/Card";
+import Feature from "../components/Feature";
 
 export default function Home({ data }) {
   if (!data || data.length === 0) {
@@ -9,15 +10,23 @@ export default function Home({ data }) {
   return (
     <>
       <section className="about-banners">
-        {data.sections.map((section, index) => (
-          <Banner key={index} data={section} index={index} />
+        {data.sections.map((item, index) => (
+          <Banner key={index} data={item} index={index} />
         ))}
       </section>
 
       <section className="stories">
         <div className="grid sm:grid-cols-2 lg:grid-cols-4">
-          {data.stories.map((story, index) => (
-            <Card key={index} cardData={story} />
+          {data.stories.map((item, index) => (
+            <Card key={index} cardData={item} />
+          ))}
+        </div>
+      </section>
+
+      <section className="features">
+        <div className="grid gap-[4rem] p-[5.5rem] md:grid-cols-3 md:gap-6">
+          {data.features.map((item, index) => (
+            <Feature key={index} featureData={item} />
           ))}
         </div>
       </section>
