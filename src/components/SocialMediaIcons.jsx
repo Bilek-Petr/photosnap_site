0a59d344909a6f93b87c672c8aa399e8ae4b2ce4
Bilek-Petr <p.bilek29@gmail.com>
2024-08-19 16:39:31 +0200

    simplified social media icons generating

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,14 @@
+export default function SocialMediaIcons({ logos }) {
+  return (
+    <div className="m-auto flex flex-row gap-3 py-10 md:row-start-3 md:py-0">
+      {logos.map((logo, index) => (
+        <img
+          key={index}
+          className="cursor-pointer brightness-0 contrast-100 invert filter transition-all duration-300 hover:filter-none"
+          src={logo}
+          alt={`Social media icon ${index + 1}`}
+        />
+      ))}
+    </div>
+  );
+}
