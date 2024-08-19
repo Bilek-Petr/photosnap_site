f57203a8f0a6425e0e33e49b8b35b134c9ed4a01
Bilek-Petr <p.bilek29@gmail.com>
2024-08-19 15:44:17 +0200

    footer responsibility finished

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -8,7 +8,7 @@ const navListStyles = cva("flex", {
       navigation:
         "custom-list-gap flex-col items-center py-6 text-[1rem] font-bold md:flex-row md:py-0 md:text-xs",
       footer:
-        "flex flex-col gap-6 py-4 text-sm text-pureWhite uppercase font-bold text-xs tracking-widest items-center",
+        "flex flex-col gap-6 py-4 text-sm text-pureWhite uppercase font-bold text-xs tracking-widest items-center md:row-span-3 md:col-start-2 md:row-start-1 md:items-start md:justify-between md:py-2 md:pl-12",
     },
   },
   defaultVariants: {
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -4,27 +4,37 @@ import NavList from "../components/NavList";
 export default function Footer({ data }) {
   return (
     <>
-      <footer className="grid justify-center bg-pureBlack py-16">
-        <div className="m-auto">
-          <img src={data.logo} alt="photosnap company logo" />
+      <footer className="bg-pureBlack py-16">
+        <div className="max-w-8xl m-auto grid w-10/12 md:grid-cols-[2fr_4fr_3fr] md:grid-rows-[min-content_7rem_min-content]">
+          {/* Logo */}
+          <div className="m-auto">
+            <img src={data.logo} alt="photosnap company logo" />
+          </div>
+
+          <div className="m-auto flex flex-row gap-3 py-10 md:row-start-3 md:py-0">
+            {data.site_logos.map((logo, index) => (
+              <img
+                key={index}
+                className="cursor-pointer brightness-0 contrast-100 invert filter transition-all duration-300 hover:filter-none"
+                src={logo}
+                alt=""
+              />
+            ))}
+          </div>
+
+          <NavList listItems={data.nav_items} variant="footer" />
+
+          <a
+            className="flex w-full justify-center py-8 text-pureWhite md:col-start-3 md:row-start-1 md:justify-end md:py-0"
+            href=""
+          >
+            {data.cta}
+          </a>
+
+          <p className="flex w-full justify-center text-lightGrey opacity-50 md:col-start-3 md:row-start-3 md:justify-end md:text-xs">
+            {data.copyright}
+          </p>
         </div>
-
-        <div className="m-auto flex flex-row gap-3 py-10">
-          {data.site_logos.map((logo) => (
-            <img
-              className="cursor-pointer brightness-0 contrast-100 invert filter transition-all duration-300 hover:filter-none"
-              src={logo}
-              alt=""
-            />
-          ))}
-        </div>
-
-        <NavList listItems={data.nav_items} variant="footer" />
-
-        <a className="m-auto py-8 text-pureWhite" href="">
-          {data.cta}
-        </a>
-        <p className="text-lightGrey opacity-50">{data.copyright}</p>
       </footer>
     </>
   );
