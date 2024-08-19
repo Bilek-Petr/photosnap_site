462e8c3a1461c658b7d06ea3d39c4d1d4c89ae7f
Bilek-Petr <p.bilek29@gmail.com>
2024-08-19 14:26:24 +0200

    finished footer version for small screens

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer({ data }) {
           <img src={data.logo} alt="photosnap company logo" />
         </div>
 
-        <div className="m-auto flex flex-row gap-3 py-8">
+        <div className="m-auto flex flex-row gap-3 py-10">
           {data.site_logos.map((logo) => (
             <img
               className="cursor-pointer brightness-0 contrast-100 invert filter transition-all duration-300 hover:filter-none"
