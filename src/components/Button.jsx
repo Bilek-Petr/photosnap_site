5a95010cdc51c7bce7f9fe2d430deb5a53557472
Bilek-Petr <p.bilek29@gmail.com>
2024-08-19 16:39:58 +0200

    CVA for arrow and rectangle buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,54 @@
-export default function Button() {
+import { cva } from "class-variance-authority";
+
+const buttonStyles = cva(
+  "inline-flex items-center justify-center font-bold uppercase transition-colors duration-300 tracking-[0.15rem]    ",
+  {
+    variants: {
+      variant: {
+        rectangle:
+          "cursor-pointer bg-pureBlack text-pureWhite  hover:bg-lightGrey hover:text-pureBlack px-5 py-3 md:text-xs text-[0.85rem]",
+        arrow: "bg-transparent text-black  text-xs",
+      },
+      size: {
+        default: "text-base",
+        large: "text-lg",
+      },
+    },
+    defaultVariants: {
+      variant: "rectangle",
+      size: "default",
+    },
+  },
+);
+
+export default function Button({
+  variant,
+  size,
+  color = "black",
+  className = "",
+  children,
+  ...props
+}) {
+  const arrowTextColor =
+    variant === "arrow" && color === "white" ? "text-white" : "text-black";
+  const arrowFilter = variant === "arrow" && color === "white" ? "invert" : "";
+
   return (
-    <>
-      <button className="cursor-pointer bg-pureBlack px-5 py-3 text-[0.85rem] font-bold uppercase tracking-[0.15rem] text-pureWhite md:text-xs">
-        Get an invite
-      </button>
-    </>
+    <button
+      className={`${buttonStyles({ variant, size })} group ${variant === "arrow" ? arrowTextColor : ""} ${className}`}
+      {...props}
+    >
+      {variant === "arrow" && (
+        <span className="relative flex items-center gap-3 group-hover:underline">
+          {children}
+          <img
+            className={`ml-2 transition-transform duration-300 group-hover:translate-x-2 ${arrowFilter}`}
+            src="/images/shared/desktop/arrow.svg"
+            alt="arrow icon"
+          />
+        </span>
+      )}
+      {variant !== "arrow" && children}
+    </button>
   );
 }
