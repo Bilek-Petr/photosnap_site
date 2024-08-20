f9e00b6770d48e0d3ab20d7ba8003e685f579a69
Bilek-Petr <p.bilek29@gmail.com>
2024-08-20 11:30:54 +0200

    pricing panels and banner finished

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,3 +1,16 @@
+import Banner from "../components/banners/Banner";
+import PricePlan from "../components/PricePlan";
+
 export default function Pricing({ data }) {
-   return <h1>Pricing page</h1>;
+  return (
+    <>
+      <section className="about-banners">
+        <Banner data={data.header} type="feature" />
+      </section>
+
+      <section className="subscription-plans">
+        <PricePlan data={data} />
+      </section>
+    </>
+  );
 }
