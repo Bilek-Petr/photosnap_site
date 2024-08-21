4e38a816caa15d2e7bc686877f16583faf4ca2ce
Bilek-Petr <p.bilek29@gmail.com>
2024-08-21 16:56:13 +0200

    setup 404 error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,21 @@
 export default function NotFound() {
-   return <h1>NotFound page</h1>;
+  return (
+    <div className="relative flex h-screen items-center justify-center overflow-hidden text-center font-bold">
+      <div className="absolute inset-0 z-0">
+        <img
+          src="/svgs/bgNotFound.svg"
+          alt="Background"
+          className="opacity-83 h-full w-full object-cover"
+        />
+      </div>
+      <div className="relative z-10">
+        <p className="text-5xl">
+          Sorry, the page you've been looking for doesn't exist
+        </p>
+        <p style={{ fontSize: "30vw" }} className="overflow-hidden">
+          404
+        </p>
+      </div>
+    </div>
+  );
 }
