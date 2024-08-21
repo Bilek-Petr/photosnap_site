63802c9ed19f6da5af92cb58f1137922659a1be3
Bilek-Petr <p.bilek29@gmail.com>
2024-08-21 11:13:02 +0200

    fixed rendering, fully responsive

--- src/components/PricePlan.jsx
+++ src/components/PricePlan.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import SubscribToggle from "./pricings/SubscribToggle";
 import SubPlan from "./pricings/SubPlan";
+import PricingTable from "./pricings/PricingTable";
 
 export default function PricePlan({ data }) {
   const [isToggled, setIsToggled] = useState(false);
@@ -9,10 +10,14 @@ export default function PricePlan({ data }) {
     <>
       <SubscribToggle isToggled={isToggled} onToggle={setIsToggled} />
 
-      <div className="m-auto flex w-11/12 max-w-screen-2xl flex-col gap-8 py-28 lg:w-10/12 lg:flex-row lg:justify-center">
+      <div className="m-auto flex w-11/12 max-w-screen-2xl flex-col gap-8 py-8 lg:w-10/12 lg:flex-row lg:justify-center">
         {data.sub_plans.map((item, index) => (
           <SubPlan key={index} subData={item} isToggled={isToggled} />
         ))}
+      </div>
+
+      <div className="compare-table">
+        <PricingTable tableData={data.price_table} />
       </div>
     </>
   );
